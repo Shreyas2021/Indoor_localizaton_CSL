187fe2933c916bdd6e8d2116304ec5768c011000
Shreyas Byndoor <byndoor2@illinois.edu>
2020-02-26 18:26:52 -0600

    removed cyan, trying to continue the skeleton black dots into white line

--- test.py
+++ test.py
@@ -77,80 +77,124 @@ def BFS(mat, i, j, x, y):
 
 
 
-img = cv2.imread("m_B148-2.png")
-img=cv2.resize(img,(900,300))
-# cv2.imshow('img',img)
-# cv2.waitKey()
-# lane = lanenet_detector()
-# lane.gradient_thresh(img)
-"""
-Apply sobel edge detection on input image in x, y direction
-"""
-# 1. Convert the image to gray scale
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# 2. Gaussian blur the image
-img = cv2.GaussianBlur(img, (3, 3), 0)
-
-# 3. Use cv2.Sobel() to find derievatives for both X and Y Axis
-grad_x = cv2.Sobel(img, cv2.CV_8U, 1, 0, ksize=3)
-grad_y = cv2.Sobel(img, cv2.CV_8U, 0, 1, ksize=3)
-
-# 4. Use cv2.addWeighted() to combine the results
-grad = cv2.addWeighted(grad_x, 0.5, grad_y, 0.5, 0)
-
-grad = cv2.convertScaleAbs(grad)
-# Apply threshold
-binary_output = cv2.threshold(grad, 25, 100,
-                                cv2.THRESH_BINARY)[1]
-                    
-for i in range(binary_output.shape[0]):
-    for j in range(binary_output.shape[1]):
-        if binary_output[i,j] == 100:
-            binary_output[i,j] = 0
-        else:
-            binary_output[i,j] = 255
-
-cv2.imshow('img',binary_output)
-cv2.waitKey()
 
 
-# path_graph = []
-# print(binary_output.shape)
-# for i in range(binary_output.shape[0]):
-#     temp = []
-#     for j in range(binary_output.shape[1]):
-#         if binary_output[i,j] == 0:
-#             temp.append(1)
-#         else:
-#             temp.append(0)
+def img_process(image):
+        
+    img=cv2.resize(image,(900,300))
+    cv2.imshow('img',img)
+    cv2.waitKey()
+    
+    # mask out the cyan parts in the image 
 
-#     path_graph.append(temp)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if (img[i,j,0]>175 & img[i,j,0] < 256) & (img[i,j,1]>175 & img[i,j,1] < 256) & (img[i,j,2]>0 & img[i,j,2] < 80):
+                img[i,j,0] = 255
+                img[i,j,1] = 255 
+                img[i,j,2] = 255 
 
-# path_graph = np.array(path_graph)
-# # print(path_graph)
+     
 
-size = np.size(binary_output)
-skel = np.zeros(binary_output.shape,np.uint8)
-element = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
-done = False 
-# Skeletonize the image 
-while True: 
-    #Step 2: Open the image
-    open_img = cv2.morphologyEx(binary_output, cv2.MORPH_OPEN, element)
-    #Step 3: Substract open from the original image
-    temp = cv2.subtract(binary_output, open_img)
-    #Step 4: Erode the original image and refine the skeleton
-    eroded = cv2.erode(binary_output, element)
-    skel = cv2.bitwise_or(skel,temp)
-    binary_output = eroded.copy()
-    # Step 5: If there are no white pixels left ie.. the image has been completely eroded, quit the loop
-    if cv2.countNonZero(binary_output)==0:
-        break
- 
-cv2.imshow("skel",skel)
+    # hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # cv2.imshow('img',hsv)
+    # cv2.waitKey()
+    
+    # # rgb mask 
+    # # lower = np.uint8([175,175,0])
+    # # upper = np.uint8([255,255,175])
 
-cv2.imshow('img',skel)
-cv2.waitKey()
+    # # hsv mask 
+    # lower = np.uint8([160,60,60])
+    # upper = np.uint8([180,100,100])
 
+    # # mask = cv2.inRange(img, lower, upper)
+    # mask = cv2.inRange(hsv, lower, upper)
+    # mask_inv = cv2.bitwise_not(mask)
+
+    # cv2.imshow('img',mask)
+    # cv2.waitKey()
+    
+    # # img = cv2.bitwise_and(img, img, mask = mask_inv)
+
+    # hsv = cv2. bitwise_and(hsv, hsv, mask = mask_inv)
+    # img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+
+    cv2.imshow('img',img)
+    cv2.waitKey()
+
+    """
+    Apply sobel edge detection on input image in x, y direction
+    """
+    # 1. Convert the image to gray scale
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # 2. Gaussian blur the image
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+
+    # 3. Use cv2.Sobel() to find derievatives for both X and Y Axis
+    grad_x = cv2.Sobel(img, cv2.CV_8U, 1, 0, ksize=3)
+    grad_y = cv2.Sobel(img, cv2.CV_8U, 0, 1, ksize=3)
+
+    # 4. Use cv2.addWeighted() to combine the results
+    grad = cv2.addWeighted(grad_x, 0.5, grad_y, 0.5, 0)
+
+    grad = cv2.convertScaleAbs(grad)
+    # Apply threshold
+    binary_output = cv2.threshold(grad, 25, 100,
+                                    cv2.THRESH_BINARY)[1]
+                        
+    for i in range(binary_output.shape[0]):
+        for j in range(binary_output.shape[1]):
+            if binary_output[i,j] == 100:
+                binary_output[i,j] = 0
+            else:
+                binary_output[i,j] = 255
+
+    cv2.imshow('img',binary_output)
+    cv2.waitKey()
+
+
+    # path_graph = []
+    # print(binary_output.shape)
+    # for i in range(binary_output.shape[0]):
+    #     temp = []
+    #     for j in range(binary_output.shape[1]):
+    #         if binary_output[i,j] == 0:
+    #             temp.append(1)
+    #         else:
+    #             temp.append(0)
+
+    #     path_graph.append(temp)
+
+    # path_graph = np.array(path_graph)
+    # # print(path_graph)
+
+    size = np.size(binary_output)
+    skel = np.zeros(binary_output.shape,np.uint8)
+    element = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
+    done = False 
+    # Skeletonize the image 
+    while True: 
+        #Step 2: Open the image
+        open_img = cv2.morphologyEx(binary_output, cv2.MORPH_OPEN, element)
+        #Step 3: Substract open from the original image
+        temp = cv2.subtract(binary_output, open_img)
+        #Step 4: Erode the original image and refine the skeleton
+        eroded = cv2.erode(binary_output, element)
+        skel = cv2.bitwise_or(skel,temp)
+        binary_output = eroded.copy()
+        # Step 5: If there are no white pixels left ie.. the image has been completely eroded, quit the loop
+        if cv2.countNonZero(binary_output)==0:
+            break
+    
+    binary_output = cv2.morphologyEx(skel, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_CROSS,(7,7)))
+    # cv2.imshow('img',skel)
+    cv2.imshow('img',binary_output)
+    cv2.waitKey()
+
+
+
+img = cv2.imread("edit_2.jpg")
+img_process(img)
 # BFS(path_graph, 0, 0,  )
